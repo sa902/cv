1b4073ddcab955e6bbe32efa7395fdf8c3e40e04
sa902 <samanderson902@gmail.com>
2021-03-22 13:41:17 +0200

    deploy

---
+++ _nuxt/static/1616413136/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcv\u002F_nuxt\u002Fstatic\u002F1616413136",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{app:{basePath:"\u002Fcv\u002F",assetsPath:"\u002Fcv\u002F_nuxt\u002F",cdnURL:a}}}}(null));
